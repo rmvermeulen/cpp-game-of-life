d33cdc469c64188752fc277739a5660defe7540e
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2018-06-02 23:33:52 +0200

    the whole thing

--- game-of-life/main.cpp
+++ game-of-life/main.cpp
@@ -17,75 +17,201 @@
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+
 // Here is a small helper for you! Have a look.
 #include "ResourcePath.hpp"
 
-int main(int, char const**)
+void game_loop(sf::RenderWindow &window,
+               std::function<void(bool)> onUpdate,
+               std::function<void(void)> onRender)
 {
-    // Create the main window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
-
-    // Set the Icon
-    sf::Image icon;
-    if (!icon.loadFromFile(resourcePath() + "icon.png")) {
-        return EXIT_FAILURE;
-    }
-    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
-
-    // Load a sprite to display
-    sf::Texture texture;
-    if (!texture.loadFromFile(resourcePath() + "cute_image.jpg")) {
-        return EXIT_FAILURE;
-    }
-    sf::Sprite sprite(texture);
-
-    // Create a graphical text to display
-    sf::Font font;
-    if (!font.loadFromFile(resourcePath() + "sansation.ttf")) {
-        return EXIT_FAILURE;
-    }
-    sf::Text text("Hello SFML", font, 50);
-    text.setFillColor(sf::Color::Black);
-
-    // Load a music to play
-    sf::Music music;
-    if (!music.openFromFile(resourcePath() + "nice_music.ogg")) {
-        return EXIT_FAILURE;
-    }
-
-    // Play the music
-    music.play();
 
     // Start the game loop
     while (window.isOpen())
     {
+        bool space = false;
         // Process events
         sf::Event event;
         while (window.pollEvent(event))
         {
             // Close window: exit
-            if (event.type == sf::Event::Closed) {
+            if (event.type == sf::Event::Closed)
+            {
                 window.close();
             }
 
             // Escape pressed: exit
-            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape) {
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Escape)
+            {
                 window.close();
+            }
+
+            if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Space)
+            {
+                space = true;
             }
         }
 
-        // Clear screen
-        window.clear();
-
-        // Draw the sprite
-        window.draw(sprite);
-
-        // Draw the string
-        window.draw(text);
+        // Update state
+        // std::cout << "before update" << std::endl;
+        onUpdate(space);
+        // std::cout << "after update" << std::endl;
 
         // Update the window
+        // std::cout << "before render" << std::endl;
+        window.clear();
+        onRender();
         window.display();
+        // std::cout << "after render" << std::endl;
     }
+}
+
+int main(int, char const **)
+{
+    const sf::Vector2f windowSize(1280.f, 1024.f);
+    const int rowCount = 200;
+    const int columnCount = 200;
+    const int cellCount = rowCount * columnCount;
+    const auto cellColor = sf::Color::Red;
+
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(windowSize.x, windowSize.y), "SFML window");
+
+    // Set the Icon
+    sf::Image icon;
+    if (!icon.loadFromFile(resourcePath() + "icon.png"))
+    {
+        return EXIT_FAILURE;
+    }
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+
+    std::vector<bool>
+        cells(cellCount);
+
+    const auto generate = [&cells] {
+        std::srand(time(0));
+        std::generate(cells.begin(), cells.end(), [] {
+            return std::rand() % 2;
+        });
+    };
+    generate();
+
+    std::vector<sf::Color const *> colors(cellCount);
+
+    const auto getCell = [&cells](int x, int y) {
+        return cells[x + y * columnCount];
+    };
+
+    const auto countNeighbours = [&cells, &getCell](int x, int y) {
+        int count = 0;
+        for (int dy = -1; dy < 2; ++dy)
+        {
+            for (int dx = -1; dx < 2; ++dx)
+            {
+                if (dx == 0 && dy == 0)
+                {
+                    continue;
+                }
+                const int nx = x + dx, ny = y + dy;
+                if (nx < 0 || nx >= columnCount || ny < 0 || ny >= rowCount)
+                {
+                    continue;
+                }
+                if (getCell(nx, ny))
+                {
+                    ++count;
+                }
+            }
+        }
+        return count;
+    };
+
+    const sf::Vector2f brushSize(windowSize.x / columnCount, windowSize.y / rowCount);
+    sf::RectangleShape cellBrush(brushSize);
+    std::cout << "brushSize: "
+              << brushSize.x << ','
+              << brushSize.y << std::endl;
+    cellBrush.setFillColor(cellColor);
+
+    int frameCount = 0;
+    int lastAlive = 0;
+    game_loop(window,
+              [&](bool spacePressed) {
+                  if (spacePressed)
+                  {
+                      generate();
+                      return;
+                  }
+
+                  std::vector<bool> updatedCells(cells);
+                  int born = 0, died = 0;
+                  for (int row = 0; row < rowCount; ++row)
+                  {
+                      for (int column = 0; column < columnCount; ++column)
+                      {
+                          const int cellIndex = column + row * columnCount;
+                          const auto neighbourCount = countNeighbours(column, row);
+                          // set color according to neighbours
+                          switch (neighbourCount)
+                          {
+                          case 2:
+                          case 3:
+                              colors[cellIndex] = &sf::Color::Green;
+                              break;
+                          default:
+                              colors[cellIndex] = &sf::Color::Red;
+                              break;
+                          }
+
+                          const bool isAlive = getCell(column, row);
+                          if (isAlive)
+                          {
+                              // Any live cell with fewer than two live neighbors dies, as if by under population.
+                              // Any live cell with two or three live neighbors lives on to the next generation.
+                              // Any live cell with more than three live neighbors dies, as if by overpopulation.
+                              if (neighbourCount < 2 || neighbourCount > 3)
+                              {
+                                  updatedCells[column + row * columnCount] = false;
+                                  ++died;
+                              }
+                          }
+                          else
+                          {
+                              // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+                              if (neighbourCount == 3)
+                              {
+                                  updatedCells[column + row * columnCount] = true;
+                                  ++born;
+                              }
+                          }
+                      }
+                  }
+                  //   std::cout
+                  //       << "born: " << born << " - "
+                  //       << "died: " << died << std::endl;
+
+                  cells = updatedCells;
+              },
+              [&] {
+                  for (int row = 0; row < rowCount; ++row)
+                  {
+                      for (int column = 0; column < columnCount; ++column)
+                      {
+                          const int cellIndex = column + row * columnCount;
+                          if (!getCell(column, row))
+                          {
+                              continue;
+                          }
+
+                          const float pos_x = brushSize.x * column;
+                          const float pos_y = brushSize.y * row;
+                          cellBrush.setPosition(pos_x, pos_y);
+                          cellBrush.setFillColor(*colors[cellIndex]);
+                          window.draw(cellBrush);
+                      }
+                  }
+              });
 
     return EXIT_SUCCESS;
 }
